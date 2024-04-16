430ccd3787517d5eb947f2b18508ced779b656bb
Jose Victor <ragjvictor@gmail.com>
2024-04-15 22:21:25 -0400

    User entity and resource

---
+++ src/main/java/com/jacazezo/course/resources/UserResource.java
@@ -0,0 +1,19 @@
+package com.jacazezo.course.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.jacazezo.course.entities.User;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+	
+	@GetMapping
+	public ResponseEntity<User> findAll(){
+		User u = new User(1L, "Joao", "joao@gmail.com", "99999999", "12345");
+		return ResponseEntity.ok().body(u);
+	}
+}
