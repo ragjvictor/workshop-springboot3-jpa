92fdf1226d00021b9c109e08d8d77b33788a14e0
Jose Victor <ragjvictor@gmail.com>
2024-04-15 23:12:54 -0400

    Service layer, component registration

---
+++ src/main/java/com/jacazezo/course/services/UserService.java
@@ -0,0 +1,26 @@
+package com.jacazezo.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.jacazezo.course.entities.User;
+import com.jacazezo.course.repositories.UserRepository;
+
+@Service
+public class UserService {
+
+	@Autowired
+	private UserRepository repository;
+
+	public List<User> findAll() {
+		return repository.findAll();
+	}
+	
+	public User findById(Long id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.get();
+	}
+}
