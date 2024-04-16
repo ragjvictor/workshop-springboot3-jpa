365f5062ab5a1ccac51014702fd294381d7ec54f
Jose Victor <ragjvictor@gmail.com>
2024-04-15 22:06:44 -0400

    Project created

---
+++ src/test/java/com/jacazezo/course/CourseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jacazezo.course;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
